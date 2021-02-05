c573d47ab6a4b87a8229a2cf28739a1cab372f77
youknowkeshi <76035490+youknowkeshi@users.noreply.github.com>
2021-02-05 18:52:30 +0700

    ggwp

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -4,13 +4,39 @@
 #include<iomanip>
 using namespace std;
 
-void showData(double *,int,int);
+void showData(double a,int N,int M){
+    cout << fixed << setprecision(2.0);
+    for(int i = 0; i < NM; i++){
+        cout << a[i];
+        if((i+1)%M==0) cout << endl;
+        else cout << " ";
+    }
+}
 
-void randData(double *,int,int);
 
-void findRowSum(const double *,double *,int,int);
+void randData(double a,int N,int M){
+    for(int i = 0; i < NM; i++){
+        a[i] = (rand()%101)/100.00;
+    }
+}
 
-void findColSum(const double *,double *,int,int);
+void findRowSum(const double *a,double *b,int N,int M){
+    for(int i=0; i < N; i++){
+        *(b+i) = 0;
+        for(int j = 0; j < M ; j++){
+            *(b+i) += (a+iM+j); 
+        }
+    }
+}
+
+void findColSum(const double *a,double *b,int N,int M){
+    for(int i=0; i < M; i++){
+        *(b+i) = 0;
+        for(int j = 0; j < N ; j++){
+            *(b+i) += (a+i+jM); 
+        }
+    }
+}
 
 int main(){
 	srand(time(0));
